a581328c6be01e4397dc3087db892fe465c24656
efforthye <efforthye@gmail.com>
2024-12-08 21:39:47 +0900

    feat: add nest g resource -

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     // constructor(private readonly configService: ConfigService){}
     // this.configService.get('PORT');
     ConfigModule.forRoot({envFilePath: `.env.${process.env.NODE_ENV}`}),
+    UsersModule,
   ],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService, ConfigService],
 })
 export class AppModule {}
