ff791ac0911644f6c2534fbef2c6d8ce6656bac8
efforthye <efforthye@gmail.com>
2024-12-08 22:15:49 +0900

    feat: request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpCode, BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -13,15 +13,19 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Res() res) {
+    const allUsers = this.usersService.findAll();
+    return res.status(200).send({allUsers});
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    // curl http://localhost:port/api/users/0 | jq
+    if(+id<1) throw new BadRequestException('id is must be greater than 0');
     return this.usersService.findOne(+id);
   }
 
+  @HttpCode(202) // 처리 중
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
