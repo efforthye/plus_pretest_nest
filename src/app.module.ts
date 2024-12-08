2eadc93376328038adb313af91fb2f3b40ab1e3e
efforthye <efforthye@gmail.com>
2024-12-08 21:30:58 +0900

    feat: add config & env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // constructor(private readonly configService: ConfigService){}
+    // this.configService.get('PORT');
+    ConfigModule.forRoot({envFilePath: `.env.${process.env.NODE_ENV}`}),
+  ],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// const env = process.env.NODE_ENV;
+// const envFile = `.env${
+//   (env=='production'||'prod')? '.prod': (env=='development'||'dev')? '.dev': '.local'
+// }`;
+// dotenv.config({
+//   path: path.resolve(envFile)
+// });
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env?.PORT || '1000';
+  console.log({port});
+  await app.listen(process.env.PORT);
 }
 bootstrap();
